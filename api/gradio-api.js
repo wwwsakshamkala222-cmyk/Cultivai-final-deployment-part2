d4e507e76d21e6aa3b89609e9aa4fe01aa5ccd9e
Saksham kala <www.sakshamkala222@gmail.com>
2025-10-09 01:48:57 +0530

    hello

--- api/gradio-api.js
+++ api/gradio-api.js
@@ -15,7 +15,7 @@ app.post("/predict", async (req, res) => {
     }
 
     const response = await fetch(
-      "https://inkiponki-plant-disease-classifier.hf.space/apipredict",
+      "https://inkiponki-plant-disease-classifier.hf.space/run/predict",
       {
         method: "POST",
         headers: { "Content-Type": "application/json" },
